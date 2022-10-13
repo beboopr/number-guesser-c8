aa8a37be97b1544efdcad6aa5e257f6aabe237c6
beboopr <linnets-router.09@icloud.com>
2022-10-13 09:35:37 -0400

    second

---
+++ guess.js
@@ -0,0 +1,7 @@
+/*
+create a program that guesses a number from 1 to 10 then ask us to guess that number.
+then it ask us to guess that number. 
+if its higher say "Too high and ask again.
+if its too low and ask again
+if its the right number say " Congratulations" and exit
+*/
