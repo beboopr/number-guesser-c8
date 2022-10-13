ceda8597503d74f3fbf5e522a4b6f046cb2c9ffe
beboopr <linnets-router.09@icloud.com>
2022-10-13 10:38:11 -0400

    guessing

--- guess.js
+++ guess.js
@@ -5,3 +5,28 @@ if its higher say "Too high and ask again.
 if its too low and ask again
 if its the right number say " Congratulations" and exit
 */
+
+import readline from "readline-sync"
+
+let userGuessedRight = false
+
+// guess a number from 1-10
+let NumberChosen = Math.ceil(Math.random() * 10)
+//console.log(NumberChosen)
+
+while (!userGuessedRight) {
+    let guess = readline.question("what is your guess?")
+    if (guess == NumberChosen) // is it equal
+    {
+        console.log("congratulations")
+        userGuessedRight = true // get out
+    }
+    if (guess > NumberChosen) {
+        console.log("too high try again")
+
+    }
+    if (guess < NumberChosen) {
+        console.log("too low try again")
+
+    }
+}
